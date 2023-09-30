4c9b1058419b44b96c3b0b6ff9284b45d52701c6
Lari <larileppanen@hotmail.com>
2023-09-30 15:45:42 +0300

    part 3.14 step 2 mongo db

--- index.js
+++ index.js
@@ -1,68 +1,41 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const persons = require('./models/persons')
+const morgan = require('morgan')
+const cors = require('cors')
+
 
 app.use(express.json())
 app.use(express.static('build'))
-const morgan = require('morgan')
-
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms '))
-
-const cors = require('cors')
-
 app.use(cors())
 
-let persons = [
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-2424-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-]
-  
-console.log(persons.length)
+
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    persons.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
-   
-    res.send( `Phonebook has info for ${persons.length} people ` + `<br/> ` + ` ${Date()}` );
+
+    res.send(`Phonebook has info for ${persons.length} people ` + `<br/> ` + ` ${Date()}`);
 
 })
 
 app.get('/api/persons/:id', (req, res) => {
 
-const id = Number(req.params.id)
-const person = persons.find(person => person.id === id)
-if (person) {
-    res.json(person)
-}
-else {
-    res.status(404).end()
-}
+    persons.findById(req.params.id).then(persons => {
+        res.json(persons)
+    })
 })
-
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     persons = persons.filter(person => person.id !== id)
-  
     res.status(204).end()
   })
 
@@ -81,36 +54,26 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    const nameCheck = persons.find(person => person.name === body.name)
-    if (nameCheck) {
+    // const nameCheck = persons.find(person => person.name === body.name)
+    // if (nameCheck) {
         
-     return res.status(400).json({
-        error:'name must be unique'
-     })   
-    }
+    //  return res.status(400).json({
+    //     error:'name must be unique'
+    //  })   
+    // }
 
-    const person = {
+    const person = new persons({
         name: body.name,
         number: body.number,
-        id:generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-
-    res.json(person)
-
+    person.save().then(savedPersons => {
+        res.json(savedPersons)
+    })
 })
 
+const PORT = process.env.PORT
 
-const generateId = () => {
-    const max = Math.floor(123456)
-    const min = Math.ceil(1983)
-    const newId = Math.floor(Math.random() * (max - min) + min) 
-    return newId
-}
-
-
-const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+if (process.argv[3] !== undefined )
+{
+person.save().then(result => {
+  mongoose.connection.close()
+})
+}
+else{
+Person.find({}).then(result => {
+    console.log("Phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
